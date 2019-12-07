9ad3470e02695d9add97675ad74a42ec12893015
Jack <doherty9@purdue.edu>
2019-12-06 21:22:49 -0500

    forogt the build dir

---
+++ frontend/build/precache-manifest.8df48629d3b03d1f6bd169f362b0b888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd00fb072b5908abb1e5740e63059f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7567a3526e3cf7ad898b",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "7567a3526e3cf7ad898b",
+    "url": "/static/js/2.abe3f967.chunk.js"
+  },
+  {
+    "revision": "2964f329796dd9af7db6",
+    "url": "/static/js/main.16494a7f.chunk.js"
+  },
+  {
+    "revision": "757a5ce5921ddcabbc85",
+    "url": "/static/js/runtime-main.665f9b4b.js"
+  }
+]);
